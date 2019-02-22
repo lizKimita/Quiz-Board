0827c579c4e4328e3bd503cde4b217695706c165
lizKimita <kimita.wanjiku@gmail.com>
2019-02-22 14:02:12 +0300

    adding the percentage conversion in js

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     var Q3 = parseInt($("input:radio[name=Question3]:checked").val());
     var Q4 = parseInt($("input:radio[name=Question4]:checked").val());
     var Q5 = parseInt($("input:radio[name=Question5]:checked").val());
-    var total = (Q1+Q2+Q3+Q4+Q5);
-    $("#marks").text(total);
+    var total = ((Q1+Q2+Q3+Q4+Q5)/25*100);
+    $("#marks").text("You scored "+ total + "%");
   });
 });
