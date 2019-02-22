e9d5d6f9f3964b15ef2ce2685f2cc841448dfa93
lizKimita <kimita.wanjiku@gmail.com>
2019-02-22 17:31:13 +0300

    adding indentation in html

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,21 @@ $(document).ready(function(){
     var Q3 = parseInt($("input:radio[name=Question3]:checked").val());
     var Q4 = parseInt($("input:radio[name=Question4]:checked").val());
     var Q5 = parseInt($("input:radio[name=Question5]:checked").val());
+
     var name=$("input#studentsname").val();
     var total = ((Q1+Q2+Q3+Q4+Q5)/25*100);
-    $("#marks").text(name+" "+"You scored "+ total + "%");
-    if (total >= 80) $("#comment").text ("Excellent Performance!");
-    else if (total <= 50)$("#comment").text ("Poor performance, please retake the test!");
+    $("#marks").text("Hello "+name+" "+",you scored "+ total + "%");
+
+    if (total >= 80) $("#comment").text ("Excellent Performance!Congratulations!");
+    else if (total <= 50)$("#comment").text ("Oh no! This is poor performance, please retake the test!");
     else $("#comment").text ("Fair performance!");
   });
+
   $("#reload").click(function(){
     $("#marks").empty();
     $("#comment").empty();
   });
+  
     $("form#stest").submit(function(event){
       event.preventDefault();
       $("#qboxes").slideDown();
