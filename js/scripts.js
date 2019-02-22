74784da8718fbf83868e14684ede6bdb6333d553
lizKimita <kimita.wanjiku@gmail.com>
2019-02-22 17:07:01 +0300

    adding the introduction form name input section

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
     var Q3 = parseInt($("input:radio[name=Question3]:checked").val());
     var Q4 = parseInt($("input:radio[name=Question4]:checked").val());
     var Q5 = parseInt($("input:radio[name=Question5]:checked").val());
+    var name=$("input#studentsname").val();
     var total = ((Q1+Q2+Q3+Q4+Q5)/25*100);
-    $("#marks").text("You scored "+ total + "%");
+    $("#marks").text(name+" "+"You scored "+ total + "%");
     if (total >= 80) $("#comment").text ("Excellent Performance!");
     else if (total <= 50)$("#comment").text ("Poor performance, please retake the test!");
     else $("#comment").text ("Fair performance!");
@@ -16,4 +17,8 @@ $(document).ready(function(){
     $("#marks").empty();
     $("#comment").empty();
   });
+    $("form#stest").submit(function(event){
+      event.preventDefault();
+      $("#qboxes").slideDown();
+       });
 });
