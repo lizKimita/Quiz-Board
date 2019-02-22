e1feab77ca237540d47ea99dd75d4951e8483fb3
lizKimita <kimita.wanjiku@gmail.com>
2019-02-22 13:47:10 +0300

    adding button and total results display

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     var Q3 = parseInt($("input:radio[name=Question3]:checked").val());
     var Q4 = parseInt($("input:radio[name=Question4]:checked").val());
     var Q5 = parseInt($("input:radio[name=Question5]:checked").val());
-    var Total = add(Q1 ,Q2 ,Q3 ,Q4 ,Q5);
+    var total = (Q1+Q2+Q3+Q4+Q5);
+    $("#marks").text(total);
   });
 });
