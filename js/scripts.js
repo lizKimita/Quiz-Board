31942e99f2161aa213444f5b44039acb8432ac94
lizKimita <kimita.wanjiku@gmail.com>
2019-02-22 15:01:41 +0300

    adding commentary to in js

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
     var Q5 = parseInt($("input:radio[name=Question5]:checked").val());
     var total = ((Q1+Q2+Q3+Q4+Q5)/25*100);
     $("#marks").text("You scored "+ total + "%");
+    if (total >= 80) $("#comment").text ("Excellent Performance!");
+    else if (total <= 50)$("#comment").text ("Poor performance, please retake the test!");
+    else $("#comment").text ("Fair performance!")
   });
 });
-$("#comment").text ("Excellent Performance")
-$("#comment").text ("Fair performance")
-$("#comment").text ("Poor performance, please retake the test")
