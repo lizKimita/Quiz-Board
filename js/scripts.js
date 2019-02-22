dd64398296d29ec62fc96118fe2c5a52d514b011
lizKimita <kimita.wanjiku@gmail.com>
2019-02-22 13:36:58 +0300

    adding and defining js variables

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("form#qboxes").submit(function(event){
+    event.preventDefault();
+    var Q1 = parseInt($("input:radio[name=Question1]:checked").val());
+    var Q2 = parseInt($("input:radio[name=Question2]:checked").val());
+    var Q3 = parseInt($("input:radio[name=Question3]:checked").val());
+    var Q4 = parseInt($("input:radio[name=Question4]:checked").val());
+    var Q5 = parseInt($("input:radio[name=Question5]:checked").val());
+    var Total = add(Q1 ,Q2 ,Q3 ,Q4 ,Q5);
+  });
+});
