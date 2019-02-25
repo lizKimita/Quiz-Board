f2714b89c41394c44597b2a9a7a8b5ad8bf27d19
lizKimita <kimita.wanjiku@gmail.com>
2019-02-25 10:53:12 +0300

    adding changes in the separation of user interface and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,28 +7,33 @@ $(document).ready(function(){
     var Q3 = parseInt($("input:radio[name=Question3]:checked").val());
     var Q4 = parseInt($("input:radio[name=Question4]:checked").val());
     var Q5 = parseInt($("input:radio[name=Question5]:checked").val());
-
+    var total = calculateScore(Q1,Q2,Q3,Q4,Q5);
     var name=$("input#studentsname").val();
-    var total = ((Q1+Q2+Q3+Q4+Q5)/25*100);
     $("#marks").text ("Hello "+name+" "+",you scored "+ total + "%");
-
-//BUSINESS LOGIC(BACKEND)
-    if (total >= 80) $("#comment").text ("Excellent Performance!Congratulations!");
-    else if (total <= 50)$("#comment").text ("Oh no! You have performed poorly, please retake the test!");
-    else $("#comment").text ("Fair performance!");
-
-    if (total <= 50) $("#reload").show();
-       else $("#reload").hide();
+    grades(total);
   });
-
-    $("#reload").click(function(){
-      $("#marks").empty();
-      $("#comment").empty();
-      $("#reload").hide();
+  $("#reload").click(function(){
+    $("#marks").empty();
+    $("#comment").empty();
+    $("#reload").hide();
   });
-
-    $("form#stest").submit(function(event){
-      event.preventDefault();
-      $("#qboxes").slideDown();
-       });
+  $("form#stest").submit(function(event){
+        event.preventDefault();
+        $("#qboxes").slideDown();
+  });
 });
+//BUSINESS LOGIC(BACKEND)
+  function calculateScore (ans1, ans2, ans3, ans4, ans5) {
+    return (ans1+ans2+ans3+ans4+ans5)/25*100;
+    };
+    
+  function grades(test) {
+    if (test >= 80) {
+      $("#comment").text ("Excellent Performance!Congratulations!");
+    } else if (test <= 50){
+      $("#comment").text ("Oh no! You have performed poorly, please retake the test!");
+      $("#reload").show();
+    } else {
+      $("#comment").text ("Fair performance!");
+    }
+  };
